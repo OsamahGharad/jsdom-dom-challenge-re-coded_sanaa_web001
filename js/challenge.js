bf6ce5581870fe2a17d707a1008dce5a14e334a1
Osamah Gharad <osamah.gharred@gmail.com>
2020-08-02 21:37:00 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -60,5 +60,5 @@ like.addEventListener("click" ,counterincrement);
     const comment= document.createElement('p');
     comment.innerHTML=commentForm.value;
     commentLists.appendChild(comment);
-    
+
   });
